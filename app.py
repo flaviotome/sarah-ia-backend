53568cf1caafa92d8a952fec1d9fbafb8962ee4a
FlavioTomeOrbitDS <flavio.tome@orbitdatascience.com>
2024-10-18 20:12:24 -0300

    Atualizacao do backend

--- app.py
+++ app.py
@@ -40,5 +40,46 @@ def getmessages():
 
 @app.route('/verifyconnected', methods=['GET'])
 def verifyconnected():
+                
+        return jsonify(result= whatsapp_scrapper.verifica_conectado())
+
+@app.route('/resetmessages', methods=['GET'])
+def resetmessages():
+                
+        return jsonify(result= whatsapp_scrapper.resetmessages())
+
+@app.route('/sendmessage', methods=['POST'])
+def sendmessage():
+    # Obtém os dados do JSON enviado pelo frontend
+    data = request.get_json()
+    
+    # Extrai os parâmetros 'nome_contato' e 'mensagem' do JSON
+    nome_contato = data.get('nome_contato')
+    mensagem = data.get('mensagem')
+    
+    # Verifica se ambos os parâmetros foram fornecidos
+    if not nome_contato or not mensagem:
+        return jsonify({"status": 400, "message": "Parâmetros 'nome_contato' e 'mensagem' são obrigatórios"}), 400
+
+    # Chama a função para enviar a mensagem
+    status_code = whatsapp_scrapper.send_message_from_frontend(nome_contato, mensagem)
+    
+    # Retorna o status com base no resultado da função
+    if status_code == 200:
+        return jsonify({"status": 200, "message": "Mensagem enviada com sucesso"}), 200
+    else:
+        return jsonify({"status": 500, "message": "Erro ao enviar a mensagem"}), 500
+
+
+@app.route('/insertmessagedataframe', methods=['POST'])
+def insertmessagedataframe():
+        # Obtém os dados do JSON enviado pelo frontend
+        data = request.get_json()
+    
+        # Extrai os parâmetros 'nome_contato' e 'mensagem' do JSON
+        nome_contato = data.get('nome_contato')
+        mensagem = data.get('mensagem')
         
-    return jsonify(result= whatsapp_scrapper.verifica_conectado())
+        whatsapp_scrapper.insert_message_dataframe(nome_contato, mensagem)        
+        return whatsapp_scrapper.getmessagesdf()
+        
--- whatsapp_scrapper.py
+++ whatsapp_scrapper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
@@ -12,11 +13,28 @@ from io import BytesIO
 import base64
 import openai_api
 
+def initial_config():
+    dir_path = os.getcwd()
+    chrome_options2 = Options()
+    #chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/profile/whatsapp")
+    chrome_options2.add_argument("--start-fullscreen")
+    driver = webdriver.Chrome(options=chrome_options2)         
+    url = "https://web.whatsapp.com"
+    driver.get(url)
+    return driver
+
+
+#******************* GLOBALS ************************
+mensagens_df = pd.DataFrame(columns=['nome_contato', 'mensagem', 'role', 'time'])
+driver = initial_config()
+STOP = False
+RUNNING = False
+
 def verifica_conectado():
     global driver
     try:
-        imagens = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "x1n2onr6.x1lliihq.xh8yej3.x5yr21d.x6ikm8r.x10wlt62.x14yjl9h.xudhj91.x18nykt9.xww2gxu.xl1xv1r.x115dhu7.x17vty23.x1hc1fzr._ao3e"))
+        conversas_text = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "x1qlqyl8.x1pd3egz.xcgk4ki"))
             )
         
         return 200
@@ -116,23 +134,32 @@ def read_last_message(driver):
     
     return last_message
 
-def insert_message_dataframe(mensagens_df, nome_contato, message):
+def insert_message_dataframe(nome_contato, message):
     """
     Função que insere uma nova mensagem enviada pelo assistente em um DataFrame.
     """
-    hora_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')    
+    global mensagens_df
+    hora_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    # Cria um dicionário com as informações da nova mensagem
     nova_mensagem = {
-        'mensagem': [message],
-        'nome_contato': [nome_contato],
-        'role': ['assistant'],
+        'mensagem': message,
+        'nome_contato': nome_contato,
+        'role': 'assistant',  # Define o role como 'assistant'
         'time': hora_atual
     }
 
-    aux_df = pd.DataFrame(data=nova_mensagem)
-    mensagens_df = pd.concat([mensagens_df, aux_df], ignore_index=True)  
-    print("Mensagem do assistente salva!")  
+    # Converte o dicionário para DataFrame
+    aux_df = pd.DataFrame([nova_mensagem])
     
+    # Concatena a nova mensagem ao DataFrame original
+    mensagens_df = pd.concat([mensagens_df, aux_df], ignore_index=True)
+    
+    #mensagens_df.to_excel("mensagens_df.xlsx")
+    
+    print("Mensagem do assistente registrada no DataFrame!")
     return mensagens_df
+
 
 def send_message(driver, nome_contato, mensagens_df, text):
     """
@@ -140,23 +167,29 @@ def send_message(driver, nome_contato, mensagens_df, text):
     """
     try:
         print("### Escrevendo resposta")
-        mensagem = openai_api.gerar_resposta_openai(mensagens_df,nome_contato)
-                
-        text_input = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]')
-        text_input.send_keys(mensagem)
-        time.sleep(0.5)
         
-        print("### Enviando resposta")
-        send_button = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')
+        mensagem = text
+                
+        # Combinando `contenteditable`, `aria-placeholder`, e `role`
+        message_box = driver.find_element(By.XPATH, "//div[@contenteditable='true' and @aria-placeholder='Digite uma mensagem' and @role='textbox']")
+        # Certifique-se que o elemento está visível
+        driver.execute_script("arguments[0].scrollIntoView(true);", message_box)
+        time.sleep(0.5)
+        # Clique e insira o texto
+        message_box.click()
+        message_box.send_keys(mensagem)
+        
+        send_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Enviar']")
         send_button.click()
         
-        # Atualiza o DataFrame com a nova mensagem enviada
-        mensagens_df = insert_message_dataframe(mensagens_df, nome_contato, mensagem)
+        
+        #insert_message_dataframe(nome_contato, mensagem)
         print("### Resposta enviada")
         
         return mensagens_df
     except Exception as e:
         print(f"Erro ao enviar a resposta da mensagem: {str(e)}")
+        return mensagens_df 
 
 def process_new_conversation(driver, mensagens_df, contact_name):
     """
@@ -183,10 +216,11 @@ def process_existing_conversation(driver, mensagens_df, contact_name):
     #mensagens_df = send_message(driver, contact_name, mensagens_df, "resposta automatica")
     return mensagens_df
 
-def verify_open_chat(driver, mensagens_df):
+def verify_open_chat(driver):
     """
     Função para verificar se há novas mensagens no chat que está aberto.
     """
+    global mensagens_df
     try:
         nome_contato = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div[4]/div/header/div[2]/div/div/div/span").text
         print(f"Conversando com {nome_contato}")
@@ -212,43 +246,7 @@ def verify_open_chat(driver, mensagens_df):
         print("Nenhuma mensagem nova na conversa")
         return mensagens_df
 
-def initial_config():
-    dir_path = os.getcwd()
-    chrome_options2 = Options()
-    chrome_options2.add_argument(r"user-data-dir=" + dir_path + "/profile/whatsapp")
-    driver = webdriver.Chrome(options=chrome_options2)
-    url = "https://web.whatsapp.com"
-    driver.get(url)
-    return driver
 
-driver = initial_config()
-STOP = False
-mensagens_df = pd.DataFrame(columns=['nome_contato', 'mensagem', 'role', 'time'])
-
-def startprocess():
-    global STOP
-    STOP = False
-    global mensagens_df
-
-    while not STOP:
-        try:
-            notificacao = driver.find_elements(By.CLASS_NAME, "x7h3shv")
-            notificacao[1].click()
-            time.sleep(0.5)
-            print(f"### Notificação encontrada!")
-
-            contact_name = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div[4]/div/header/div[2]/div/div/div/span").text
-
-            if mensagens_df[mensagens_df['nome_contato'] == contact_name].empty:
-                mensagens_df = process_new_conversation(driver, mensagens_df, contact_name)
-            else:
-                mensagens_df = process_existing_conversation(driver, mensagens_df, contact_name)
-
-        except Exception as e:
-            print("Aguardando notificação...")
-            time.sleep(0.5)
-
-        mensagens_df = verify_open_chat(driver, mensagens_df)
 
 def stopprocess():
     global STOP
@@ -262,8 +260,104 @@ def getmessagesdf():
 
     # Agora você pode converter para JSON sem problemas
     try:
-        json_data = mensagens_df.to_json()
+        
+        #json_data = mensagens_df.to_json()
+        json_data = mensagens_df.to_json(orient="records")
         return json_data
     except ValueError as e:
         print(f"Erro ao converter para JSON: {e}")
+        
         return None
+    
+
+def search_contact_by_name(driver,name): 
+    print("Procurando Contato")   
+    #Insere no name na caixa de busca do whatsapp
+    caixa_pesquisa = driver.find_element(By.CLASS_NAME, "x1hx0egp.x6ikm8r.x1odjw0f.x6prxxf.x1k6rcq7.x1whj5v")
+    #caixa_pesquisa.clear()
+     # Método alternativo 1: Seleciona e apaga o texto existente
+    caixa_pesquisa.send_keys(Keys.CONTROL + 'a')  # Ctrl + A para selecionar tudo (ou Command + A no macOS)
+    caixa_pesquisa.send_keys(Keys.BACKSPACE)      # Apaga o texto selecionado
+    caixa_pesquisa.send_keys(name)
+
+    time.sleep(0.5)
+
+    #procura o resultado que possui o name e clica no elemento
+    resultados_da_pesquisa = driver.find_element(By.CLASS_NAME, 'x1y332i5.x1n2onr6.x6ikm8r.x10wlt62')
+    
+    xpath = f"//span[@title='{name}']"
+    #xpath = f"//span[@title=normalize-space('{name}')]"
+    busca = resultados_da_pesquisa.find_element(By.XPATH, xpath)
+    print("Contato Encontrado")    
+    print(busca.text)
+    
+    # Scrolling the element into view
+    driver.execute_script("arguments[0].scrollIntoView(true);", busca)
+    time.sleep(0.5)  # Small pause to allow for scroll completion
+
+    busca.click()
+    
+    return busca
+
+def send_message_from_frontend(nome_contato, mensagem):
+    global mensagens_df
+    global driver
+    
+    try:
+        #procura o contato na caixa de pesquisa
+        search_contact_by_name(driver,nome_contato)
+        time.sleep(0.5)
+        #Manda mensagem para o contato
+        mensagens_df = send_message(driver, nome_contato, mensagens_df, mensagem)
+        
+        return 200
+    except Exception as e:
+        print(e)
+        return 500
+    
+    
+def startprocess():
+    global STOP
+    global mensagens_df
+    global RUNNING
+    
+    if RUNNING == True:
+        print("Já está rodadando!")
+        return None
+    
+    STOP = False                
+    RUNNING = True
+
+    print("### Iniciando o processo!")
+    while not STOP:
+        try:
+            notificacao = driver.find_elements(By.CLASS_NAME, "x7h3shv")
+            notificacao[1].click()
+            time.sleep(0.5)
+            print(f"### Notificação encontrada!")
+
+            contact_name = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div[4]/div/header/div[2]/div/div/div/span").text
+
+            if mensagens_df[mensagens_df['nome_contato'] == contact_name].empty:
+                print(f"### Novo Contato!")
+                #mensagens_df = process_new_conversation(driver, mensagens_df, contact_name)
+                mensagens_df = process_existing_conversation(driver, mensagens_df, contact_name)
+            else:
+                print(f"### Contato existente {contact_name}")
+                mensagens_df = process_existing_conversation(driver, mensagens_df, contact_name)
+
+        except Exception as e:            
+            time.sleep(0.5)
+
+        mensagens_df = verify_open_chat(driver)
+        
+    
+def resetmessages():
+    global mensagens_df
+    
+    try:
+        mensagens_df = pd.DataFrame(columns=['nome_contato', 'mensagem', 'role', 'time'])
+    except:
+        return 500
+    
+    return 200
